135b00e8ee6b19908e86b80ea4aadcaa2105058a
胡林浩 <1540694044@qq.com>
2020-12-04 09:53:04 +0800

    提交

--- src/topic0_100/Solution10.java
+++ src/topic0_100/Solution10.java
@@ -1,21 +1,18 @@
-//package topic0_100;
-//
-///**
-// * 正则表达式匹配
-// */
-//class Solution10 {
-//    public boolean isMatch(String s, String p) {
-//        int sLength = s.length();
-//        int pLength = p.length();
-//        boolean[][] f = new boolean[sLength][pLength];
-//        f[0][0] = true;
-//        for (int i = 0; i < pLength; i++) {
-//            char c = s.charAt(i);
-//            for (int j = i; j < pLength; j++) {
-//                if (c == p.charAt(j)) {
-//                    f[i][j]=
-//                }
-//            }
-//        }
-//    }
-//}
+package topic0_100;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 正则表达式匹配
+ */
+class Solution10 {
+    public boolean isMatch(String s, String p) {
+        
+    }
+
+    @Test
+    public void test() {
+        boolean match = isMatch("ab", ".*");
+        System.out.println(match);
+    }
+}
---
+++ src/topic0_100/Solution23.java
@@ -0,0 +1,9 @@
+//package topic0_100;
+//
+//import commmon.ListNode;
+//
+//public class Solution23 {
+//    public ListNode mergeKLists(ListNode[] lists) {
+//
+//    }
+//}
