23eb426da2b28f259b8e7f32e4ac38bc999faa68
胡林浩 <1540694044@qq.com>
2021-04-20 18:03:09 +0800

    代码提交

--- src/Solution1690.java
+++ src/Solution1690.java
@@ -3,29 +3,26 @@ import org.junit.jupiter.api.Test;
 public class Solution1690 {
     public int stoneGameVII(int[] stones) {
         int n = stones.length;
-        int[][] dp = new int[n + 2][n + 2];
-        for (int i = 0; i < n + 2; i++) {
-            dp[0][i] = i % 2 == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            if (i % 2 != 0) {
+                dp[0][i] = dp[0][i - 1] + stones[i - 1];
+                dp[i][0] = dp[i - 1][0] + stones[n - i];
+            } else {
+                dp[0][i] = dp[0][i - 1] - stones[i - 1];
+                dp[i][0] = dp[i - 1][0] - stones[n - i];
+            }
         }
         for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                if (i == 1 && j == 1) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                boolean isMax = ((i + j) % 2 == 1);
-                if (isMax) {
-                    dp[i][j] = Math.max(dp[i - 1][j] + stones[i - 1], dp[i][j - 2] + stones[n + 1 - j]);
+            for (int j = 1; j < n + 1 - i; j++) {
+                if (i + j % 2 == 0) {
+                    dp[i][j] = Math.min(dp[i - 1][j] - stones[i - 1], dp[i][j - 1] - stones[n - j]);
                 } else {
-                    dp[i][j] = Math.min(dp[i - 1][j] - stones[i - 1], dp[i][j - 2] - stones[n + 1 - j]);
+                    dp[i][j] = Math.max(dp[i - 1][j] + stones[i - 1], dp[i][j - 1] + stones[n - j]);
                 }
             }
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i < n + 2; i++) {
-            min = Math.min(dp[i][n + 2 - i], min);
-        }
-        return min;
+        return 0;
     }
 
     @Test
